5b2c4d8f72283f26c21d496a43903210cb930f6a
DickyR15 <125186855+DickyR15@users.noreply.github.com>
2026-09-25 17:16:01 +0800

    fix: escape template diagnostics for Windows console

--- scripts/inspect_template.py
+++ scripts/inspect_template.py
@@ -13,4 +13,5 @@ for pat in [r'<[^>]+id=["\']device["\'][^>]*>',
     print("PATTERN",pat)
     for m in list(re.finditer(pat,t,re.I))[:20]:
         print("----",m.start())
-        print(repr(t[max(0,m.start()-500):m.end()+1500].replace("\n"," ")[:3000]))
+        snippet=t[max(0,m.start()-500):m.end()+1500].replace("\n"," ")[:3000]
+        print(snippet.encode("unicode_escape").decode("ascii"))
